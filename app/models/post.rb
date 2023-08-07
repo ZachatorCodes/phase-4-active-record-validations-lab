3887760662a4523fcbc4cafae9846c618c773e5b
Zach Schwartz <zachatorcodes@gmail.com>
2023-08-07 17:10:02 -0600

    Finished lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+
+  validate :check_if_clickbait
+
+    CLICKBAIT_TITLES = [
+      # REGEX
+
+      # Regex statements are in between the / characters
+      # The i at the end means ignore case, or case-insensitive
+      # \d means digit, so any digit from 1-9
+
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d/i,
+      /Guess/i
+    ]
+
+  def check_if_clickbait
+    if CLICKBAIT_TITLES.none? { |pattern| pattern.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
